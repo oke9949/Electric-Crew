f342650d8e15b7e8aad188015f23882614a052b8
oke9949 <oke9949@gmail.com>
2026-08-21 15:05:09 +0200

    chore: set app version to 10.0.3

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "10.0.2"
-export const RELEASE_NAME = "Android In-App Updates"
+export const APP_VERSION = "10.0.3"
+export const RELEASE_NAME = "In-App Update Test Release"
